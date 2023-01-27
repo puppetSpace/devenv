96a36a15ec79c53d1ed2adc99e2beae7fa9002fe
puppetSpace <mobscene_dude@hotmail.com>
2023-01-27 13:00:45 +0100

    Add files via upload

---
+++ SetUpEnv.ps1
@@ -0,0 +1,58 @@
+$envlocation = "~/.devenv";
+if(-Not (Test-Path -Path $envlocation)){
+    New-Item -ItemType Directory  $envlocation;
+}
+
+function DownloadFromGithub{
+    wget "https://github.com/puppetSpace/terminalthemes/raw/main/Caskaydia%20Cove%20Nerd%20Font%20Complete.ttf" -O $envlocation;
+    wget "https://raw.githubusercontent.com/puppetSpace/terminalthemes/main/puppet.omp.json" -O $envlocation;
+}
+
+function InstallFont{
+    $font = Get-Item -Path "$envlocation/Caskaydia%20Cove%20Nerd%20Font%20Complete.ttf";
+    if (-not(Test-Path "C:\Windows\Fonts\$($font.Name)")) {
+        $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+        $destination.CopyHere($font,0x10)
+    }
+}
+
+function SetPowershellProfile{
+    if (-not(Test-Path $PROFILE)) {
+        "oh-my-posh init pwsh --config $envlocation/puppet.omp.json | Invoke-Expression
+        Set-Alias -Name kc -Value kubectl
+        " | Out-File $PROFILE
+    }
+}
+
+function InstallWinget{
+    $existing = Get-AppPackage -name "Microsoft.Winget.Source"
+
+    if($existing -ne $null){
+        wget "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -O $envlocation;
+        Add-AppPackage -path "$envlocation/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle";
+    }
+}
+
+function InstallOhMyPosh{
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+}
+
+function InstallWingetPackages{
+    winget install Microsoft.WindowsTerminal
+}
+
+function InstallWsl{
+    wsl --install
+}
+
+
+DownloadFromGithub;
+InstallFont;
+InstallWinget
+InstallOhMyPosh;
+SetPowershellProfile;
+InstallWingetPackages;
+
+
+
+
