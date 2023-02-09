40bdb690284345b5fba6469023c7a308412cdc3d
puppetSpace <mobscene_dude@hotmail.com>
2023-02-09 20:37:58 +0100

    Update SetUpEnv.ps1

--- SetUpEnv.ps1
+++ SetUpEnv.ps1
@@ -4,12 +4,12 @@ if(-Not (Test-Path -Path $envlocation)){
 }
 
 function DownloadFromGithub{
-    wget "https://github.com/puppetSpace/terminalthemes/raw/main/Caskaydia%20Cove%20Nerd%20Font%20Complete.ttf" -O $envlocation;
-    wget "https://raw.githubusercontent.com/puppetSpace/terminalthemes/main/puppet.omp.json" -O $envlocation;
+    wget "https://github.com/puppetSpace/terminalthemes/raw/main/Caskaydia%20Cove%20Nerd%20Font%20Complete.ttf" -Outfile "$envlocation/Caskaydia.ttf";
+    wget "https://raw.githubusercontent.com/puppetSpace/terminalthemes/main/puppet.omp.json" -OutFile "$envlocation/puppet.omp.json";
 }
 
 function InstallFont{
-    $font = Get-Item -Path "$envlocation/Caskaydia%20Cove%20Nerd%20Font%20Complete.ttf";
+    $font = Get-Item -Path "$envlocation/Caskaydia.ttf";
     if (-not(Test-Path "C:\Windows\Fonts\$($font.Name)")) {
         $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
         $destination.CopyHere($font,0x10)
